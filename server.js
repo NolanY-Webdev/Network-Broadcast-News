f026f373380cd1b08e5d53c44ca83a3b17c417fa
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-15 16:56:38 -1000

    created server and client files, setup basic net server in server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var net = require('net');
+var PORT = 6969
+var server = net.createServer(function(c) {
+  console.log('client connected');
+  c.on('end', function() {
+    console.log('client disconnected');
+  });
+  c.write('hello\r\n');
+  c.pipe(c);
+});
+server.listen(PORT, function() {
+  console.log('server bound');
+});
+
+server.createConnection(PORT, '0.0.0.0');
