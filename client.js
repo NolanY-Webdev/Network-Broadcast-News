27ecef073f0fd0619017f19d08d25055644be3ed
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-16 12:20:04 -1000

    got some neat things working like user IDs and disconnection messages

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 var net = require('net');
 var PORT = 6969;
 var HOST = '0.0.0.0';
+var user = '';
 var client = new net.Socket();
 client.connect({ port : PORT, host : HOST },
   function() {
@@ -8,7 +9,7 @@ client.connect({ port : PORT, host : HOST },
 
     process.stdin.on('data', function(data) {
       console.log('sent: ' + data);
-      client.write('recieved: ' + data);
+      client.write('said: ' + data);
     });
   });
 
--- server.js
+++ server.js
@@ -4,23 +4,28 @@ var IP = '0.0.0.0';
 var sockets = [];
 
 var server = net.createServer(function(socket) {
-  console.log('client connected');
+  console.log('client connected USER ID:' + socket._handle.fd);
   sockets.push(socket);
-
+  var UID = socket._handle.fd;
 
 
   socket.on('data', function(data) {
     var i;
     for ( i = 0; i < sockets.length; i++ ) {
       if ( sockets[i] === socket ) continue;
-      sockets[i].write(data.toString());
+      sockets[i].write('USER: ' + socket._handle.fd + ' ' + data.toString());
     }
   });
 
   socket.on('end', function() {
-    console.log('client disconnected');
+    console.log('client disconnected USER: ' + UID);
     var i = sockets.indexOf(socket);
     sockets.splice(i, 1);
+    for ( i = 0; i < sockets.length; i++ ) {
+      if ( sockets[i] === socket ) continue;
+      sockets[i].write('USER: ' + UID + ' has left the channel');
+    }
+
   });
 
 });
