8cba2113715114ae9da570095b9521075f98d461
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-16 14:42:36 -1000

    Usernames fully functional, allow users to change names at will

--- server.js
+++ server.js
@@ -6,24 +6,31 @@ var sockets = [];
 var server = net.createServer(function(socket) {
   console.log('client connected USER ID:' + socket._handle.fd);
   sockets.push(socket);
-  var UID = socket._handle.fd;
-
+  var userID = socket._handle.fd;
+  sockets[sockets.length - 1].userID = userID;
+  sockets[sockets.length - 1].write('To change your User ID Type \'RENAME <new name here>\'');
 
   socket.on('data', function(data) {
-    var i;
-    for ( i = 0; i < sockets.length; i++ ) {
-      if ( sockets[i] === socket ) continue;
-      sockets[i].write('USER: ' + socket._handle.fd + ' ' + data.toString());
+
+    for (var i = 0; i < sockets.length; i++ ) {
+      if (data.toString().substring(0, 13) == 'said: RENAME ') {
+        var newname = data.toString().slice( 13 );;
+        sockets[sockets.indexOf(socket)].userID = newname;
+        userID = newname;
+      } else {
+        if ( sockets[i] === socket ) continue;
+        sockets[i].write('USER: ' + userID + ' ' + data.toString());
+      }
     }
   });
 
   socket.on('end', function() {
-    console.log('client disconnected USER: ' + UID);
+    console.log('client disconnected USER: ' + userID);
     var i = sockets.indexOf(socket);
     sockets.splice(i, 1);
     for ( i = 0; i < sockets.length; i++ ) {
       if ( sockets[i] === socket ) continue;
-      sockets[i].write('USER: ' + UID + ' has left the channel');
+      sockets[i].write('USER: ' + userID + ' has left the channel');
     }
 
   });
